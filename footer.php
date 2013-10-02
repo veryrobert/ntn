8b0f65f2531e8034f841122c31d42363a3bb2d23
veryrobert <hello@robertfarrelly.com>
2013-10-02 16:39:52 +0100

    Updated with parrallax and image title hover

--- footer.php
+++ footer.php
@@ -1,15 +1,15 @@
 
+</div>
+ <!-- Closing of wrap from header -->
+
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.1.min.js"><\/script>')</script>
         <script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
+        <script src="http://www.appelsiini.net/projects/lazyload/jquery.lazyload.min.js?v=1.8.5" charset="utf-8"></script>
+        <script src="https://rawgithub.com/markdalgleish/stellar.js/master/jquery.stellar.min.js" charset="utf-8"></script>
         <script src="js/plugins.js"></script>
    
-
-        <script>
-            var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
-            (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
-            g.src='//www.google-analytics.com/ga.js';
-            s.parentNode.insertBefore(g,s)}(document,'script'));
-        </script>
     </body>
 </html>
+
+
--- index.php
+++ index.php
@@ -1,16 +1,52 @@
 <?php include('header.php') ?>
    
 
-<div class="container">
+<section class="container">
     
-    <div class="images"><img src="img/image-01.png" height="432" width="644"></div>
-    <div class="images"><img src="img/image-02.png" height="644" width="455"></div>
-    <div class="images"><img src="img/image-03.png" height="304" width="454"></div>
-    <div class="images"><img src="img/image-04.png" height="508" width="359"></div>
-    <div class="images"><img src="img/image-05.png" height="242" width="361"></div>
-    <div class="images"><img src="img/image-06.png" height="376" width="266"></div>
+    <div data-stellar-ratio="0.6" class="images one">
+      <a href="#">
+      <p class="imglink">This is title</p>
+      <img class="lazy" src="img/white.png" data-original="img/image-01.png" height="432" width="644">
+      </a>
+    </div>
+    
+    <div data-stellar-ratio="0.7" class="images two">
+      <a href="#">
+      <p class="imglink">This is title</p>
+      <img class="lazy" src="img/white.png" data-original="img/image-02.png" height="644" width="455">
+      </a>
+    </div>
+    
+    <div data-stellar-ratio="0.6" class="images three">
+      <a href="#">
+      <p class="imglink">This is title</p>
+      <img class="lazy" src="img/white.png" data-original="img/image-03.png" height="304" width="454">
+      </a>   
+    </div>
+    
+    <div data-stellar-ratio="0.7" class="images four">
+      <a href="#">
+      <p class="imglink">This is title</p>
+      <img class="lazy" src="img/white.png" data-original="img/image-04.png" height="508" width="359">
+    </a>
+    </div>
+    
+    <div data-stellar-ratio="0.7" class="images five">
+      <a href="#">
+        <p class="imglink">This is title</p>
+        <img class="lazy" src="img/white.png" data-original="img/image-05.png" height="242" width="361">
+      </a>
+    </div>
+    
+    <div data-stellar-ratio="0.6" class="images six">
+      <a href="#">
+        <p class="imglink">This is title</p>
+        <img class="lazy" src="img/white.png" data-original="img/image-06.png" height="376" width="266">
+      </a>
+    </div>
    
-</div>
+</section>
+
 
 
 <?php include('footer.php') ?>
